8576ba69e51aaa43caf2fea76dd2f375d75c6906
Sega1A <sergio.garciacuchallo@gmail.com>
2026-04-13 19:46:18 -0400

    update: merge

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ from db import db
 
 app = FastAPI(title="Tu Tiendita Server")
 
+
 @app.get("/")
 async def read_root():
     return {"message": "Servidor funcionando"}
 
+
 @app.get("/test-db")
 async def test_db():
     coleccion = db["productos"]
-    resultado = await coleccion.insert_one({
-        "nombre": "Producto prueba",
-        "precio": 10
-    })
+    resultado = await coleccion.insert_one({"nombre": "Producto prueba", "precio": 10})
 
     return {
         "message": "Conexión exitosa a MongoDB",
-        "inserted_id": str(resultado.inserted_id)
+        "inserted_id": str(resultado.inserted_id),
     }
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,10 +7,4 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
-
-
-def test_read_item():
-    response = client.get("/items/5?q=prueba")
-    assert response.status_code == 200
-    assert response.json() == {"item_id": 5, "q": "prueba"}
+    assert response.json() == {"message": "Servidor funcionando"}
